1505cd8ba2a7469a8447987627196251a05ea8a8
Scott Hallock <scott@hallocks.net>
2023-09-06 22:53:36 +0000

    A not-badly-brittle way to parse Chase's not-CSV

--- scrape-chase-activity.py
+++ scrape-chase-activity.py
@@ -11,11 +11,28 @@ def main():
         activity_soup = BeautifulSoup(f, 'html.parser')
 
     criteria = {
-        'id': re.compile('^ACTIVITY+'),
+        'id': re.compile('^ACTIVITY.+'),
         'class': 'mds-activity-table__row',
     }
     for row in activity_soup.find_all(attrs=criteria):
         csvish = row.get('data-values')
+        components = csvish.split('$')
+        if (len(components) != 2):
+            raise SyntaxError(
+                'More than one $ character in record: {}'.format(csvish))
+
+        other_components = components[0].split(',')
+        date = other_components[0]
+        print('Date: {}'.format(date))
+
+        description = ''.join(other_components[1:-2])
+        print('Description: {}'.format(description))
+
+        amount_components = components[1].split(',')
+        print('Negative Amount: {}'.format(
+            ''.join(amount_components[:len(amount_components) - 1])))
+
+        print()
 
 
 if __name__ == "__main__":
