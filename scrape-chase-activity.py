6c41c1acc16c4fd855b719c5cdc288035bf710b8
Scott Hallock <scott@hallocks.net>
2023-09-07 03:29:42 +0000

    A decent parse

--- scrape-chase-activity.py
+++ scrape-chase-activity.py
@@ -10,6 +10,8 @@ def main():
     with open('activity.html', encoding='utf-8') as f:
         activity_soup = BeautifulSoup(f, 'html.parser')
 
+    output_list = [["Type", "Transaction Date", "Post Date", "Description",
+                    "Amount", "Negative Amount", "Additional Description", "Category"]]
     criteria = {
         'id': re.compile('^ACTIVITY.+'),
         'class': 'mds-activity-table__row',
@@ -22,17 +24,24 @@ def main():
                 'More than one $ character in record: {}'.format(csvish))
 
         other_components = components[0].split(',')
+        if (len(other_components) < 3):
+            raise SyntaxError('Too few fields in record: {}'.format(csvish))
+
         date = other_components[0]
-        print('Date: {}'.format(date))
 
         description = ''.join(other_components[1:-2])
-        print('Description: {}'.format(description))
 
         amount_components = components[1].split(',')
-        print('Negative Amount: {}'.format(
-            ''.join(amount_components[:len(amount_components) - 1])))
+        negative_amount = ''.join(
+            amount_components[:len(amount_components) - 1])
 
-        print()
+        output_list.append(
+            ["Sale", date, date, description, "", negative_amount, "", ""])
+
+    with open('activity.csv', 'w', newline='') as output_file:
+        activity_writer = csv.writer(output_file)
+        for row in output_list:
+            activity_writer.writerow(row)
 
 
 if __name__ == "__main__":
