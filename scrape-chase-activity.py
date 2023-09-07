6b7b6ce4a0c8ff36a59d3c4d5858cdbc2b87deb7
Scott Hallock <scott@hallocks.net>
2023-09-07 20:21:47 +0000

    Covert to class

--- scrape-chase-activity.py
+++ scrape-chase-activity.py
@@ -1,65 +1,27 @@
 # /usr/bin/env python3
 import argparse
-import csv
-import re
-
-from bs4 import BeautifulSoup
+import scrape.chase
 
 
-def get_html_table_rows(file):
-    activity_soup = BeautifulSoup(file, 'html.parser')
-    criteria = {
-        'id': re.compile('^ACTIVITY.+'),
-        'class': 'mds-activity-table__row',
-    }
-    return activity_soup.find_all(attrs=criteria)
+class ChaseOutputFormatter:
+    def __init__(self):
+        pass
 
+    def format_header(self):
+        return ['Type', 'Transaction Date', 'Post Date', 'Description',
+                'Amount', 'Negative Amount', 'Additional Description', 'Category']
 
-def get_record_string(row):
-    return row.get('data-values')
-
-
-def parse_row(record_string):
-    components = record_string.split('$')
-    if (len(components) != 2):
-        raise SyntaxError(
-            'More than one $ character in record: {}'.format(record_string))
-
-    other_components = components[0].split(',')
-    if (len(other_components) < 3):
-        raise SyntaxError('Too few fields in record: {}'.format(record_string))
-    date = other_components[0]
-    description = ''.join(other_components[1:-2])
-
-    amount_components = components[1].split(',')
-    if (len(amount_components) < 2):
-        raise SyntaxError('Too few fields in record: {}'.format(record_string))
-    negative_amount = ''.join(amount_components[:len(amount_components) - 1])
-
-    return {
-        'date': date,
-        'description': description,
-        'negative_amount': negative_amount,
-    }
-
-
-def output_row(record):
-    return [
-        'Sale',
-        record['date'],
-        record['date'],
-        record['description'],
-        '',
-        record['negative_amount'],
-        '',
-        '',
-    ]
-
-
-def output_csv_records(file, output_record_list):
-    csv_writer = csv.writer(file)
-    for record in output_record_list:
-        csv_writer.writerow(record)
+    def format_output_record(self, output_record):
+        return [
+            'Sale',
+            output_record['date'],
+            output_record['date'],
+            output_record['description'],
+            '',
+            output_record['negative_amount'],
+            '',
+            '',
+        ]
 
 
 def main():
@@ -69,17 +31,11 @@ def main():
     parser.add_argument('output_csv', help='output CSV file')
     args = parser.parse_args()
 
-    with open(args.input_html, encoding='utf-8') as i:
-        table_rows = get_html_table_rows(i)
-
-    output_record_list = [['Type', 'Transaction Date', 'Post Date', 'Description',
-                           'Amount', 'Negative Amount', 'Additional Description', 'Category']]
-    for table_row in table_rows:
-        record_string = get_record_string(table_row)
-        output_record_list.append(output_row(parse_row(record_string)))
-
-    with open(args.output_csv, 'w', newline='') as o:
-        output_csv_records(o, output_record_list)
+    with open(args.input_html, encoding='utf-8') as input_html:
+        with open(args.output_csv, 'w', newline='') as output_csv:
+            chase_scraper = scrape.chase.Chase(
+                input_html, output_csv, ChaseOutputFormatter())
+            chase_scraper.scrape()
 
 
 if __name__ == "__main__":
---
+++ scrape/chase.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import csv
+import re
+
+from bs4 import BeautifulSoup
+
+
+class Chase:
+    def __init__(self, input_html, output_csv, output_formatter):
+        self.input_html = input_html
+        self.output_csv = output_csv
+        self.output_csv_writer = csv.writer(self.output_csv)
+        self.output_formatter = output_formatter
+
+    def get_html_table_rows(self):
+        activity_soup = BeautifulSoup(self.input_html, 'html.parser')
+        criteria = {
+            'id': re.compile('^ACTIVITY.+'),
+            'class': 'mds-activity-table__row',
+        }
+        return activity_soup.find_all(attrs=criteria)
+
+    def get_record_string(self, row):
+        return row.get('data-values')
+
+    def parse_input_record_string(self, input_record_string):
+        components = input_record_string.split('$')
+        if (len(components) != 2):
+            raise SyntaxError(
+                'More than one $ character in record: {}'.format(input_record_string))
+
+        other_components = components[0].split(',')
+        if (len(other_components) < 3):
+            raise SyntaxError(
+                'Too few fields in record: {}'.format(input_record_string))
+        date = other_components[0]
+        description = ''.join(other_components[1:-2])
+
+        amount_components = components[1].split(',')
+        if (len(amount_components) < 2):
+            raise SyntaxError(
+                'Too few fields in record: {}'.format(input_record_string))
+        negative_amount = ''.join(
+            amount_components[:len(amount_components) - 1])
+
+        return {
+            'date': date,
+            'description': description,
+            'negative_amount': negative_amount,
+        }
+
+    def scrape(self):
+        input_table_rows = self.get_html_table_rows()
+        self.output_csv_writer.writerow(self.output_formatter.format_header())
+        for input_table_row in input_table_rows:
+            input_record_string = self.get_record_string(input_table_row)
+            output_record = self.parse_input_record_string(input_record_string)
+            self.output_csv_writer.writerow(
+                self.output_formatter.format_output_record(output_record))
